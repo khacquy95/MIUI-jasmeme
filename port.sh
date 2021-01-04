db99b898035b4f131038115847f700d68278ad51
khacquy95 <67647822+khacquy95@users.noreply.github.com>
2021-01-04 11:32:13 +0800

    Update port.sh

--- port.sh
+++ port.sh
@@ -9,9 +9,7 @@ git -C $CURRENTDIR submodule update --init --recursive
 if [[ -z $STOCKTAR ]] || [[ -z $PORTZIP ]] || [[ -z $SOURCEROM ]]
 then
 echo "usage: 
-port.sh [zip to be ported]  [tar of stock rom of the same android version] [android source of same version]
-example:
-port.sh ~/xiaomi.eu.zip ~/jasmine_global_images.tgz ~/aicp" && exit
+port.sh https://mgb1.androidfilehost.com/dl/8INLuawyPmSC3k7v2r1OvQ/1609817464/17248734326145706204/xiaomi.eu_multi_HMNote7_20.12.28_v12-10.zip? https://bigota.d.miui.com/V11.0.19.0.QDIMIXM/jasmine_global_images_V11.0.19.0.QDIMIXM_20201218.0000.00_10.0_9d37383b8a.tgz ~/aicp" && exit
 fi
 if [ $CURRENTUSER == root ]
 then
